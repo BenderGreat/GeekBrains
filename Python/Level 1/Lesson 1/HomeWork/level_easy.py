b27d1f6f53051f15e72ed7911ab6229c788ba291
BenderGreat <bendergreate@gmail.com>
2018-09-05 21:03:37 +0300

    Add decision for second task

--- Python/Level 1/Lesson 1/HomeWork/level_easy.py
+++ Python/Level 1/Lesson 1/HomeWork/level_easy.py
@@ -1,22 +1,23 @@
 # Задача-1: поработайте с переменными, создайте несколько,
 # выведите на экран, запросите от пользователя и сохраните в переменную, выведите на экран
 
-first_var = 'Моя первая переменная Pyton' # first variable type str
-second_var = 2 # second variable type int
-third_var = True # third variable type bool
-
-print('first_var = ', first_var, ' Type = ', type(first_var))
-print('first_var = ', second_var, ' Type = ', type(second_var))
-print('first_var = ', third_var, ' Type = ', type(third_var))
+# first_var = 'Моя первая переменная Pyton'  # first variable type str
+# second_var = 2  # second variable type int
+# third_var = True  # third variable type bool
+#
+# print('\nfirst_var = ', first_var, ' Type = ', type(first_var))
+# print('\nfirst_var = ', second_var, ' Type = ', type(second_var))
+# print('\nfirst_var = ', third_var, ' Type = ', type(third_var))
+#
+# first_input = input('\nВведите любые сиволы ')
+# print('\nВы ввели ',first_input, '\nТип данных', type(first_input))
 
 # Задача-2: Запросите от пользователя число, сохраните в переменную,
 # прибавьте к числу 2 и выведите результат на экран.
 # Если возникла ошибка, прочитайте ее, вспомните урок и постарайтесь устранить ошибку.
 
-
-
-
-
+first_input = int(input('\nВведите любое число '))
+print('\nВы ввели ',first_input, '\nприбавим к числу 2 \nИтого: ', first_input + 2)
 
 # Задача-3: Запросите у пользователя его возраст.
 # Если ему есть 18 лет, выведите: "Доступ разрешен",
